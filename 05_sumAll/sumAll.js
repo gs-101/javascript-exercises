1e1555a002f043b32404256cc757c6c74601b2de
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-05-03 18:03:31 -0300

    05_sumAll: complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumLoop = function(start, end) {
+  let result = 0;
 
+  for (let i = start; i <= end; i++) {
+    result += i;
+  }
+
+  return result;
+}
+
+const sumAll = function(left, right) {
+  let error = 'ERROR';
+
+  for (const arg of arguments) {
+    if (arg < 0 || !Number.isInteger(arg)) {
+      return error;
+    }
+  }
+
+  if (left < right) {
+    return sumLoop(left, right);
+  } else {
+    return sumLoop(right, left);
+  }
 };
 
 // Do not edit below this line
