da14c6d7b7d38a5c45526664381de6391f625756
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-05-03 14:25:52 -0300

    02_repeatString: complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+  let result = '';
 
+  if (times < 0) {
+    return 'ERROR'; // Not an actual error.
+  }
+
+  for (let i = 1; i <= times; i ++) {
+    result += str;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
