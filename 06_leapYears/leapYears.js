e3e91802a27acec48254c68d6b84431652919dc3
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-05-03 18:14:54 -0300

    06_leapYears: complete exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const divisibleByFour = function(number) {
+  return number % 4 == 0;
+}
 
+const divisibleByOneHundred = function(number) {
+  return number % 100 == 0;
+}
+
+const divisibleByFourHundred = function(number) {
+  return number % 400 == 0;
+}
+
+const leapYears = function(year) {
+  return divisibleByFour(year) && !divisibleByOneHundred(year) || divisibleByFourHundred(year);
 };
 
 // Do not edit below this line
