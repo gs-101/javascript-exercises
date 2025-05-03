40255c762e5fc462dde76b1af45f9939c7623594
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-05-03 17:41:13 -0300

    04_removeFromArray: complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+  let filter = array.filter((item) => !values.includes(item));
+  
+  return filter;
 };
 
 // Do not edit below this line
