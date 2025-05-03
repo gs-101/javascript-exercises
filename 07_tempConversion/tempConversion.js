15124525396dae23f82bf716e1c517b13780e6b3
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-05-03 18:20:47 -0300

    07_tempConversion: complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const roundtoFirstDecimal = function(number) {
+  return Math.round(number * 10) / 10;
+}
+
+const convertToCelsius = function(fahrenheit) {
+  let result = (fahrenheit - 32) * (5 / 9);
+
+  // We've been asked to round it down.
+  return roundtoFirstDecimal(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result = (celsius * (9 / 5) + 32);
+
+  // We've been asked to round it down.
+  return roundtoFirstDecimal(result);  
 };
 
 // Do not edit below this line
